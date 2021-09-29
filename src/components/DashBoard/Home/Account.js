4d8f2dcc193bc42335aedb56671476a58db06812
jdiazcoto <jfdc69@gmail.com>
2021-09-29 11:22:55 -0600

    Went ahead and worked on dashboard home

--- src/components/DashBoard/Home/Account.js
+++ src/components/DashBoard/Home/Account.js
@@ -3,16 +3,77 @@ import React from "react";
 const Account = () => {
   return (
     <div>
-      <div className="card ">
+      <div className="card mt-5">
+        <h5 className="card-title py-2">Account</h5>
         <div className="row">
           <div className="col">
-            <div className="card-body">Content</div>
+            <div className="card-body">Description</div>
           </div>
           <div className="col">
-            <div className="card-body">Content</div>
+            <div className="card-body">Product</div>
           </div>
           <div className="col">
-            <div className="card-body">Content</div>
+            <div className="card-body">Balance</div>
+          </div>
+          <div className="col">
+            <div className="card-body">Logs</div>
+          </div>
+          <div className="col">
+            <div className="card-body">Transfer</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="card-body">HN-BANK ACCOUNT</div>
+          </div>
+          <div className="col">
+            <div className="card-body">7777777</div>
+          </div>
+          <div className="col">
+            <div className="card-body">100 LPS</div>
+          </div>
+          <div className="col">
+            <div className="card-body">-></div>
+          </div>
+          <div className="col">
+            <div className="card-body">-></div>
+          </div>
+        </div>
+      </div>
+      <div className="card mt-5">
+        <h5 className="card-title py-2">Account</h5>
+        <div className="row">
+          <div className="col">
+            <div className="card-body">Description</div>
+          </div>
+          <div className="col">
+            <div className="card-body">Product</div>
+          </div>
+          <div className="col">
+            <div className="card-body">Balance</div>
+          </div>
+          <div className="col">
+            <div className="card-body">Logs</div>
+          </div>
+          <div className="col">
+            <div className="card-body">Transfer</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="card-body">US-BANK ACCOUNT</div>
+          </div>
+          <div className="col">
+            <div className="card-body">7777777</div>
+          </div>
+          <div className="col">
+            <div className="card-body">97 USD</div>
+          </div>
+          <div className="col">
+            <div className="card-body">-></div>
+          </div>
+          <div className="col">
+            <div className="card-body">-></div>
           </div>
         </div>
       </div>
